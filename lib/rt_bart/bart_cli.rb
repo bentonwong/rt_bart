989c2cb0ee1be85f6dbd4b3e78705e0b67332486
bentonwong <bentonwong@gmail.com>
2017-03-01 16:03:18 -0800

    fixed stations and added current time

--- lib/rt_bart/bart_cli.rb
+++ lib/rt_bart/bart_cli.rb
@@ -2,64 +2,59 @@ class RtBartCLI
 
   def call
     welcome
-    get_station
-    scrape
-    display
   end
 
   def welcome
     puts "Real Time BART.gov Departures\n"
-    puts "The current time is: " + Time.now
+    puts "Current time is #{Time.now}"
   end
 
-  def station_codes
-    station_codes = {12th=>"12th St. Oakland City Center",
-16th=>"16th St. Mission (SF)",
-19th=>"19th St. Oakland",
-24th=>"24th St. Mission (SF)",
-ashb=>"Ashby (Berkeley)",
-balb=>"Balboa Park (SF)",
-bayf=>"Bay Fair (San Leandro)",
-cast=>"Castro Valley",
-civc=>"Civic Center (SF)",
-cols=>"Coliseum",
-colm=>"Colma",
-conc=>"Concord",
-daly=>"Daly City",
-dbrk=>"Downtown Berkeley",
-dubl=>"Dublin/Pleasanton",
-deln=>"El Cerrito del Norte",
-plza=>"El Cerrito Plaza",
-embr=>"Embarcadero (SF)",
-frmt=>"Fremont",
-ftvl=>"Fruitvale (Oakland)",
-glen=>"Glen Park (SF)",
-hayw=>"Hayward",
-lafy=>"Lafayette",
-lake=>"Lake Merritt (Oakland)",
-mcar=>"MacArthur (Oakland)",
-mlbr=>"Millbrae",
-mont=>"Montgomery St. (SF)",
-nbrk=>"North Berkeley",
-ncon=>"North Concord/Martinez",
-oakl=>"Oakland Int'l Airport",
-orin=>"Orinda",
-pitt=>"Pittsburg/Bay Point",
-phil=>"Pleasant Hill",
-powl=>"Powell St. (SF)",
-rich=>"Richmond",
-rock=>"Rockridge (Oakland)",
-sbrn=>"San Bruno",
-sfia=>"San Francisco Int'l Airport",
-sanl=>"San Leandro",
-shay=>"South Hayward",
-ssan=>"South San Francisco",
-ucty=>"Union City",
-warm=>"Warm Springs/South Fremont",
-wcrk=>"Walnut Creek",
-wdub=>"West Dublin",
-woak=>"West Oakland"}
-
+  def stations
+    stations = [{:code => "12th", :station => "12th St. Oakland City Center"},
+            {:code => "16th", :station => "16th St. Mission (SF)"},
+            {:code => "19th", :station => "19th St. Oakland"},
+            {:code => "24th", :station => "24th St. Mission (SF)"},
+            {:code => "ashb", :station => "Ashby (Berkeley)"},
+            {:code => "balb", :station => "Balboa Park (SF)"},
+            {:code => "bayf", :station => "Bay Fair (San Leandro)"},
+            {:code => "cast", :station => "Castro Valley"},
+            {:code => "civc", :station => "Civic Center (SF)"},
+            {:code => "cols", :station => "Coliseum"},
+            {:code => "colm", :station => "Colma"},
+            {:code => "conc", :station => "Concord"},
+            {:code => "daly", :station => "Daly City"},
+            {:code => "dbrk", :station => "Downtown Berkeley"},
+            {:code => "dubl", :station => "Dublin/Pleasanton"},
+            {:code => "deln", :station => "El Cerrito del Norte"},
+            {:code => "plza", :station => "El Cerrito Plaza"},
+            {:code => "embr", :station => "Embarcadero (SF)"},
+            {:code => "frmt", :station => "Fremont"},
+            {:code => "ftvl", :station => "Fruitvale (Oakland)"},
+            {:code => "glen", :station => "Glen Park (SF)"},
+            {:code => "hayw", :station => "Hayward"},
+            {:code => "lafy", :station => "Lafayette"},
+            {:code => "lake", :station => "Lake Merritt (Oakland)"},
+            {:code => "mcar", :station => "MacArthur (Oakland)"},
+            {:code => "mlbr", :station => "Millbrae"},
+            {:code => "mont", :station => "Montgomery St. (SF)"},
+            {:code => "nbrk", :station => "North Berkeley"},
+            {:code => "ncon", :station => "North Concord/Martinez"},
+            {:code => "oakl", :station => "Oakland Int'l Airport"},
+            {:code => "orin", :station => "Orinda"},
+            {:code => "pitt", :station => "Pittsburg/Bay Point"},
+            {:code => "phil", :station => "Pleasant Hill"},
+            {:code => "powl", :station => "Powell St. (SF)"},
+            {:code => "rich", :station => "Richmond"},
+            {:code => "rock", :station => "Rockridge (Oakland)"},
+            {:code => "sbrn", :station => "San Bruno"},
+            {:code => "sfia", :station => "San Francisco Int'l Airport"},
+            {:code => "sanl", :station => "San Leandro"},
+            {:code => "shay", :station => "South Hayward"},
+            {:code => "ssan", :station => "South San Francisco"},
+            {:code => "ucty", :station => "Union City"},
+            {:code => "warm", :station => "Warm Springs/South Fremont"},
+            {:code => "wcrk", :station => "Walnut Creek"},
+            {:code => "wdub", :station => "West Dublin"},
+            {:code => "woak", :station => "West Oakland"}]
   end
-
 end
