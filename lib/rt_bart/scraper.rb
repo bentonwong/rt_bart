1f08e1f2b6301ab2c364d3c246e512037e33f7d8
bentonwong <bentonwong@gmail.com>
2017-03-07 08:53:16 -0800

    refactored scraper.rb

--- lib/rt_bart/scraper.rb
+++ lib/rt_bart/scraper.rb
@@ -4,54 +4,57 @@ require "Nokogiri"
 
 class Scraper
 
-KEY = "ZKZB-PQE6-92VT-DWE9" #BART.gov API Developer Key required to access its APIs
+  KEY = "ZKZB-PQE6-92VT-DWE9" #BART.gov API Developer Key required to access its APIs
 
-  def self.scrape_api(station_code) #used by self.get_line_destinations & self.get_train_status
-    edt_api = "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=#{station_code}&key=#{KEY}"
-    doc = Nokogiri::HTML(open(edt_api))
+  def self.scrape_api(suffix) #used by self.get_line_destinations & self.get_train_status
+    api_url = "http://api.bart.gov/api/#{suffix}"
+    doc = Nokogiri::HTML(open(api_url))
   end
 
   def self.get_line_destinations(station_code) #gets the real time updates
-    doc = Scraper.scrape_api(station_code)
-    doc.css("station etd").css("abbreviation").collect {|x| x.text}
+    suffix = "etd.aspx?cmd=etd&orig=#{station_code}&key=#{KEY}"
+    doc = Scraper.scrape_api(uffix)
+    doc.css("station etd").css("abbreviation").collect {|code| code.text}
   end
-
+  
+#------------------------------------------------------
   def self.get_train_status(station_code) #gets real time departure times
-    doc = Scraper.scrape_api(station_code)
-    output = doc.css('station etd').map do |etd|
+    suffix = "etd.aspx?cmd=etd&orig=#{station_code}&key=#{KEY}"
+    doc = Scraper.scrape_api(suffix)
+    doc.css('station etd').map do |values|
       {
-        destination: etd.css('destination').text,
-        abbreviation: etd.css('abbreviation').text,
-        arrivals: reformat(etd.css('estimate').map { |e| e.css('minutes').text.to_i }),
-        cars: (etd.css('estimate').map { |e| e.css('length').text.to_i })
+        destination: values.css('destination').text,
+        abbreviation: values.css('abbreviation').text,
+        arrivals: reformat(values.css('estimate').map { |minute| minute.css('minutes').text.to_i }),
+        cars: (values.css('estimate').map { |length| length.css('length').text.to_i })
       }
     end
-    output
   end
 
-  def self.reformat(eta) #reformats the minutes data to show 'now' when it is 0
-     eta.map do |time|
-       time == 0 ? 'now' : "#{time}"
-     end
+  def self.reformat(etd) #reformats the minutes data to show 'now' when it is 0
+    etd.map do |time|
+      time == 0 ? 'now' : "#{time}"
+    end
   end
+
+#------------------------------------------------------
 
   def self.get_station_info(station_code) #get station info as provided by BART
-    stnt_info_api = "http://api.bart.gov/api/stn.aspx?cmd=stnaccess&orig=#{station_code}&l=1&key=#{KEY}"
-    doc = Nokogiri::HTML(open(stnt_info_api))
-    output = doc.css("//stations").text
-    output
+    suffix = "stn.aspx?cmd=stnaccess&orig=#{station_code}&l=1&key=#{KEY}"
+    doc = Scraper.scrape_api(suffix)
+    doc.css("//stations").text
   end
 
-  def self.scrape_advisory(station_code) #gets system advisories
-    adv_api = "http://api.bart.gov/api/bsa.aspx?cmd=bsa&orig=#{station_code}&key=#{KEY}"
-    doc = Nokogiri::XML(open(adv_api))
-    advisories = doc.search('description').children.find{|e| e.cdata?}.text
+  def self.scrape_advisory(station_code) #gets system advisories; uses XML
+    advisory_url = "http://api.bart.gov/api/bsa.aspx?cmd=bsa&orig=#{station_code}&key=#{KEY}"
+    doc = Nokogiri::XML(open(advisory_url))
+    doc.search('description').children.find{|adv| adv.cdata?}.text
   end
 
   def self.scrape_train_count #gets the current count of the trains currently running in the system
-    tc_api = "http://api.bart.gov/api/bsa.aspx?cmd=count&key=#{KEY}"
-    doc = Nokogiri::HTML(open(tc_api))
-    count = doc.css("traincount").text
+    suffix = "bsa.aspx?cmd=count&key=#{KEY}"
+    doc = Scraper.scrape_api(suffix)
+    doc.css("traincount").text
   end
 
 end
