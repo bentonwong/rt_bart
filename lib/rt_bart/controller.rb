b59c696db72ecb07e1b6b00d89b47fdcb42d103a
bentonwong <bentonwong@gmail.com>
2017-03-07 09:21:54 -0800

    refactored controller.rb

--- lib/rt_bart/controller.rb
+++ lib/rt_bart/controller.rb
@@ -61,14 +61,23 @@ class Controller
   end
 
 #--------------------------------------------------------
+
+  def run #entry point into the controller from the bin file
+    welcome
+    handle_request
+    goodbye
+  end
+
+#--------------------------------------------------------
+
   def welcome #provides a nice welcome message with the current time.
     puts "\nReal Time BART.gov Train Departures"
     puts "\nCurrent time is #{Time.now}"
   end
-#--------------------------------------------------------
+
   def get_input #returns valid BART station code; for complete list, go to http://api.bart.gov/docs/overview/abbrev.aspx
     if !Station.all.empty?
-      print "\n(Recent searches: #{Station.all.collect {|x| x.name}})\n"
+      print "\n(Recent searches: #{Station.all.collect {|stn| stn.name}})\n"
     end
 
     valid_station = false #sets the flag for the loop below
@@ -76,16 +85,18 @@ class Controller
     while valid_station == false #checks to make sure the user is entering a valid station
       puts "\nEnter departure station code (e.g. \'woak\' for West Oakland) for real time departure information (type 'list' for codes):"
       station = gets.strip.downcase
-      if station == "list"
-        display_stations
-      elsif STATIONS.detect {|x| x[:code] == station} #validates response using chart above
-        valid_station = true
-      else
-        puts "\n ALERT! Invalid station code -> try again"
-      end
+        if station == "list"
+          display_stations
+        elsif STATIONS.detect {|stn| stn[:code] == station} #validates response using chart above
+          valid_station = true
+        else
+          puts "\n ALERT! Invalid station code -> try again"
+        end
     end
+
     station #returns the station value
   end
+
 #-----------------------------------------------------------
   def handle_request #controls the fulfillment of the query; because the fulfillment process is complex, this method was written to simplify and better manage the calls to the various processes.
     done = false
@@ -96,26 +107,18 @@ class Controller
       done = check_if_done
     end
   end
+#--------------------------------------------------------
 
   def process_request(station) #determines if the station request already has an instance; if it does, it does not create another one. regardless of the outcome, a call is made to the station class to get updates from the BART api
-    if !Station.all.detect {|x| x.instance_variable_get("@name") == station}
+    if !Station.all.detect {|instance| instance.instance_variable_get("@name") == station}
       @station_instance = Station.new(station) #creates a new station instance if it does not already exist in the Station.all
     else
-      @station_instance = Station.all.detect {|x| x.instance_variable_get("@name") == station}
+      @station_instance = Station.all.detect {|instance| instance.instance_variable_get("@name") == station}
     end
       station_instance.call(station)
   end
 
-  def converter(status_data) #converts the status data into a more user friendly, easier to read format
-    status_data.each do |x|
-      puts ">>Destination: #{x[:destination]} (#{x[:abbreviation]})" #displays destination info
-
-      combined_array = x[:arrivals].zip(x[:cars]) #combines the arrivals and cars arrays so they can be displayed together below
-      combined_array.each do |x|
-        puts "#{x[0]} min (#{x[1]} cars)" #displays the arrivals and cars information
-      end
-    end
-  end
+#----------------------------------------------------------------------
 
   def display_results(station) #displays real time station data returned from the station instance
     puts "\n#{station.upcase} departures as of #{Time.now}\n"
@@ -127,21 +130,30 @@ class Controller
     puts "Data provided by BART.gov. Real time train departure data available at https://www.bart.gov/schedules/eta."
   end
 
-  def check_if_done #checks if the user is done with this session and checks for valid responses
-    input_validator = false
-    info_request = false
-    while input_validator == false && info_request == false
-      puts "\nCheck another station? [\'y\'/\'n\' or \'i\' for above station information]"
+  def converter(status_data) #converts the status data into a more user friendly, easier to read format
+    status_data.each do |values|
+      puts ">>Destination: #{values[:destination]} (#{values[:abbreviation]})" #displays destination info
+      combined_array = values[:arrivals].zip(values[:cars]) #combines the arrivals and cars arrays so they can be displayed together below
+      combined_array.each do |data|
+        puts "#{data[0]} min (#{data[1]} cars)" #displays the arrivals and cars information
+      end
+    end
+  end
 
+  def check_if_done #checks if the user is done with this session and checks for valid responses
+    input_check = false
+    while input_check == false
+      puts "\nCheck another station? [\'y\'/\'n\' or \'i\' for above station information]"
       check = gets.strip.downcase
-      if check == 'y' || check == 'n'
-        input_validator = true
+
+      if ['y', 'n'].include?(check)
+        input_check = true
       elsif check == 'i'
         puts "\n#{station_instance.info}"
-        done = false
       else
         puts "\nALERT! Invalid response --> type \'y\'/\'n\' or \'i\'"
       end
+
     end
 
     if check == 'y'
@@ -149,7 +161,7 @@ class Controller
     else
       done = true
     end
-    done
+
   end
 
 #----------------------------------------------------------------------
@@ -164,11 +176,5 @@ class Controller
   end
 
 #-----------------------------------------------------------------------
-
-  def run #entry point into the controller from the bin file
-    welcome
-    handle_request
-    goodbye
-  end
 
 end
